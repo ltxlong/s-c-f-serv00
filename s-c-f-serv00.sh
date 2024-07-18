8ce298cacf48b9b77f0fe66a8a35e463b328e761
UiLgNoD-lIaMtOh <UiLgNoD.lIaMtOh@hotmail.com>
2024-07-19 01:48:03 +0800

    Update s-c-f-serv00.sh

--- s-c-f-serv00.sh
+++ s-c-f-serv00.sh
@@ -1969,15 +1969,23 @@ downloadFile() {
     rm -rfv $FILENAME usr
 
     # 下载官方 cloundflare https://freebsd.pkgs.org/14/freebsd-amd64/cloudflared-2023.10.0_2.pkg.html
+    #URI="https://pkg.freebsd.org/FreeBSD:14:amd64/latest/All/cloudflared-2023.10.0_2.pkg"
+    #FILENAME=$(basename $URI)
+    #wget -t 3 -T 10 --verbose --show-progress=on --progress=bar --no-check-certificate --hsts-file=/tmp/wget-hsts -c "${URI}" -O $FILENAME
+    #FILEPATH=$(tar tvf $FILENAME | grep bin/cloudflared | awk '{print $9}')
+    #tar zxvf $FILENAME
+    #mv -fv .$FILEPATH ${HOME}/s-c-f-serv00-${REPORT_DATE_S}/cloudflared-freebsd
+    #chmod -v u+x ${HOME}/s-c-f-serv00-${REPORT_DATE_S}/cloudflared-freebsd
+    #rm -rfv $FILENAME usr
     # 非官方 https://cloudflared.bowring.uk/binaries/cloudflared-freebsd-latest.7z
     URI="https://cloudflared.bowring.uk/binaries/cloudflared-freebsd-latest.7z"
     FILENAME=$(basename $URI)
     wget -t 3 -T 10 --verbose --show-progress=on --progress=bar --no-check-certificate --hsts-file=/tmp/wget-hsts -c "${URI}" -O $FILENAME
-    FILEPATH=$(tar tvf $FILENAME | grep bin/cloudflared | awk '{print $9}')
+    FILEPATH=$(tar tvf $FILENAME | grep temp/cloudflared | awk '{print $9}')
     tar zxvf $FILENAME
     mv -fv .$FILEPATH ${HOME}/s-c-f-serv00-${REPORT_DATE_S}/cloudflared-freebsd
     chmod -v u+x ${HOME}/s-c-f-serv00-${REPORT_DATE_S}/cloudflared-freebsd
-    rm -rfv $FILENAME usr
+    rm -rfv $FILENAME temp
 }
 
 killMe() {
