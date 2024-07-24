55af88420c861f73a8572f531d469f94be4d658f
UiLgNoD-lIaMtOh <UiLgNoD.lIaMtOh@hotmail.com>
2024-07-24 12:32:14 +0800

    Update s-c-f-serv00.sh

--- s-c-f-serv00.sh
+++ s-c-f-serv00.sh
@@ -1989,7 +1989,7 @@ UiLgNoD-lIaMtOh
 
 # 下载 sing-box-freebsd cloudflared-freebsd 配置并启用
 downloadFile() {
-    # 下载 freebsd 社区 yuri@FreeBSD.org sing-box 包 sing-box https://freebsd.pkgs.org/14/freebsd-amd64/sing-box-1.9.3.pkg.html
+    # 下载 freebsd 社区 yuri@FreeBSD.org 的 sing-box 包 sing-box https://freebsd.pkgs.org/14/freebsd-amd64/sing-box-1.9.3.pkg.html
     URI="https://pkg.freebsd.org/FreeBSD:14:amd64/latest/All/sing-box-1.9.3.pkg"
     FILENAME=$(basename $URI)
     wget -t 3 -T 10 --verbose --show-progress=on --progress=bar --no-check-certificate --hsts-file=/tmp/wget-hsts -c "${URI}" -O $FILENAME
@@ -1998,22 +1998,8 @@ downloadFile() {
     mv -fv .$FILEPATH ${HOME}/s-c-f-serv00-${REPORT_DATE_S}/sing-box-freebsd
     chmod -v u+x ${HOME}/s-c-f-serv00-${REPORT_DATE_S}/sing-box-freebsd
     rm -rfv $FILENAME usr
-    # k0baya 版 https://raw.githubusercontent.com/k0baya/sb-for-serv00/main/sing-box
-    #URI="https://raw.githubusercontent.com/k0baya/sb-for-serv00/main/sing-box"
-    #FILENAME=$(basename $URI)
-    #wget -t 3 -T 10 --verbose --show-progress=on --progress=bar --no-check-certificate --hsts-file=/tmp/wget-hsts -c "${URI}" -O ${HOME}/s-c-f-serv00-${REPORT_DATE_S}/sing-box-freebsd
-    #chmod -v u+x ${HOME}/s-c-f-serv00-${REPORT_DATE_S}/sing-box-freebsd
 
-    # 下载官方 cloundflare https://freebsd.pkgs.org/14/freebsd-amd64/cloudflared-2023.10.0_2.pkg.html
-    #URI="https://pkg.freebsd.org/FreeBSD:14:amd64/latest/All/cloudflared-2023.10.0_2.pkg"
-    #FILENAME=$(basename $URI)
-    #wget -t 3 -T 10 --verbose --show-progress=on --progress=bar --no-check-certificate --hsts-file=/tmp/wget-hsts -c "${URI}" -O $FILENAME
-    #FILEPATH=$(tar tvf $FILENAME | grep bin/cloudflared | awk '{print $9}')
-    #tar zxvf $FILENAME
-    #mv -fv .$FILEPATH ${HOME}/s-c-f-serv00-${REPORT_DATE_S}/cloudflared-freebsd
-    #chmod -v u+x ${HOME}/s-c-f-serv00-${REPORT_DATE_S}/cloudflared-freebsd
-    #rm -rfv $FILENAME usr
-    # 非官方 https://cloudflared.bowring.uk/binaries/cloudflared-freebsd-latest.7z
+    # 非官方构建 cloudflared-freebsd https://cloudflared.bowring.uk/binaries/cloudflared-freebsd-latest.7z
     URI="https://cloudflared.bowring.uk/binaries/cloudflared-freebsd-latest.7z"
     FILENAME=$(basename $URI)
     wget -t 3 -T 10 --verbose --show-progress=on --progress=bar --no-check-certificate --hsts-file=/tmp/wget-hsts -c "${URI}" -O $FILENAME
